0885871a9bdcc38e2b85966b46f79bdde665ba75
mzenzi21 <husseinmzenzi831@gmail.com>
2025-04-28 18:21:11 +0300

    initial commit

---
+++ nextjs-dashboard/app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+import React from 'react'
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+  
+}
---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+         <h1 className = "text-center text-purple-700 text-2xl">Customer Page</h1>
+         
+         </div>
+  )
+}
